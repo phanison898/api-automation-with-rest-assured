95a2c41799c36b39031a255cf67caaaa995ea780
phanison898 <phanison898@gmail.com>
2024-03-16 02:17:21 +0530

    added maven project

---
+++ src/main/java/com/base/Base.java
@@ -0,0 +1,45 @@
+package com.base;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+public class Base {
+
+	private JsonObject jsonObject = null;
+
+	Base() {
+
+		String jsonFilePath = System.getProperty("user.dir") + "/config.json";
+
+		if (jsonObject != null) {
+			FileReader reader = null;
+			Gson gson = new Gson();
+
+			try {
+				reader = new FileReader(jsonFilePath);
+				jsonObject = gson.fromJson(reader, JsonObject.class);
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} finally {
+				if (reader != null) {
+					try {
+						reader.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
+	}
+
+	public String getApiUrl() {
+		return jsonObject.get("api_url").getAsString();
+	}
+
+}
